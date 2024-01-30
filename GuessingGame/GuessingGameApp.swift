89cc5ae3e59cc36a1c2265230698ab8c9335e6b4
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-30 14:36:27 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Fiona ZHOU on 2024-01-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
