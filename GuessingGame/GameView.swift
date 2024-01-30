8c05193586ab0874710cbc35e6a492e1efd0d75e
Xinlin Zhou <XZhou@lcs.on.ca>
2024-01-30 14:38:19 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by Fiona ZHOU on 2024-01-30.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
